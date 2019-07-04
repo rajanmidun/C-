d4c479e2be57c1aea2dde071d662273413244549
rajanmidun <43414436+rajanmidun@users.noreply.github.com>
2019-07-04 21:46:22 +0545

    Add files via upload

---
+++ 1-Basic Programs/Area of the circle.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+#define pi 3.1415
+int main()
+{
+    float radius;
+    cout <<"Enter the radius of the circle\n";
+    cin >>radius;
+    cout <<"The area is " <<pi*radius*radius;
+    return 0;
+}
+
+
